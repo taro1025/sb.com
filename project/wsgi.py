eae16921ed28653f536ebaa5cfdda6cc2f21c426
taro1025 <game4967@gmail.com>
2020-12-23 19:49:02 +0900

    alt wsgi.py project_name > project

--- project/wsgi.py
+++ project/wsgi.py
@@ -12,6 +12,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project_name.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = Cling(get_wsgi_application())
