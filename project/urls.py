d067f4dca9d1d8aca49462f8467d327aa7ebbefb
taro1025 <game4967@gmail.com>
2020-12-25 19:17:21 +0900

    delete social django

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('match.urls')),
-    path('', include('social_django.urls')),
 
 ]
 
