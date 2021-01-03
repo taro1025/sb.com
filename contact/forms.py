f5c1ca0db100259f451e0daa4a5bc608577b7bdd
taro1025 <game4967@gmail.com>
2021-01-03 18:02:06 +0900

    add agreement

--- contact/forms.py
+++ contact/forms.py
@@ -5,17 +5,20 @@ class ContactForm(forms.Form):
 
     name = forms.CharField(
         label='お名前', max_length=50,
-        required=False, help_text='※任意'
+        required=False, help_text='※任意',
+        widget=forms.TextInput(attrs={'class':'form-control'})
     )
 
     email = forms.EmailField(
-        label = 'メールアドレス', required=False, help_text='※任意'
+        label = 'メールアドレス', required=False, help_text='※任意',
+        widget=forms.TextInput(attrs={'class':'form-control'})
     )
 
     text = forms.CharField(
-        label='お問い合わせ内容', widget=forms.Textarea
+        label='お問い合わせ内容', widget=forms.Textarea(attrs={'class':'form-control'})
         )
 
     category = forms.CharField(
-        label='件名', required=False, max_length=100, help_text='※任意'
+        label='件名', required=False, max_length=100, help_text='※任意',
+        widget=forms.TextInput(attrs={'class':'form-control'})
         )
