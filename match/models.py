9469932e5a9cceb829100afac378d333bb86ad5a
taro1025 <game4967@gmail.com>
2021-01-06 05:21:27 +0900

    totonoeta

--- match/models.py
+++ match/models.py
@@ -85,16 +85,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     course3 = models.IntegerField('コース3', blank=True, null=True)
 
     describe_text="""(以下はテンプレートです。)
-・値段
+    ・値段
 
-
-・コースの内容
+    ・コースの内容
     （対戦してフィードバックする、リプレイを送ってもらってフィードバックする、
     質問をする権利を○日まで与える。。。などコース内容は自由に決めても良いですが、
     永久コンサル、戦闘力〇〇まで上げる等はトラブルの元になりやすいのでなるべくお控えください。）
 
 
-・コースを終える基準（〜日まで、○回戦するまで、○本のリプレイを送ってもらうまで、等。
+    ・コースを終える基準（〜日まで、○回戦するまで、○本のリプレイを送ってもらうまで、等。
     明確な終了条件を設けるようにお願いします。）"""
 
     describe1 = models.TextField('コース1の説明' ,blank=True, default=describe_text)
