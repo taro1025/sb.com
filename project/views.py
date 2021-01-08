74fca371ea2558840c8d86b16c90d7503ee4d9d1
taro1025 <game4967@gmail.com>
2021-01-08 11:14:07 +0900

    sate_map complete

--- project/views.py
+++ project/views.py
@@ -5,17 +5,8 @@ from .sitemaps import (
 from django.contrib.sitemaps.views import sitemap,index
 from django.views import generic
 from django.shortcuts import redirect,render
-site = {
-    'top': BlogPostSitemap,
-    'about': StaticViewSitemap,
-}
-import boto3
-from django.conf import settings
 
 
-def SiteMap(request):
-
-    return redirect('https://smash-match.s3-ap-northeast-1.amazonaws.com/site_map.xml')
 
 
 def robot(request):
