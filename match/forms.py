0ecd08e66b6febf791937c0d6b645149207221d7
taro1025 <game4967@gmail.com>
2021-01-22 21:50:53 +0900

    last

--- match/forms.py
+++ match/forms.py
@@ -113,11 +113,7 @@ class UserCreateForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
 
-    template = """(大まかな構成)&#13;&#10;
-    ・値段&#13;&#10;
-    ・コースの内容(教え方は自由です。)&#13;&#10;
-    ・コースを終える基準(トラブルを避けるため、なるべく定量的な基準を設けてください。)&#13;&#10;
-    ・コース履行中の連絡手段(Line, Discord, Twitter等)&#13;&#10;"""
+    template = """コースの内容, コースを終える基準, コース履行中の連絡手段など"""
 
 
     describe1 = forms.CharField(widget=forms.Textarea(attrs={
